a81f3ee47455b4fa8266085f99b66bc9da32e96a
Akira <19719195+devakira@users.noreply.github.com>
2018-08-12 12:46:43 -0400

    New site

--- assets/nav.js
+++ assets/nav.js
@@ -2,8 +2,8 @@ var isOpen = false;
 
 function moveCtxMenu() {
     var pos = $("#more").position();
-    $(".logged-in-ctx").css("top", pos.top + 20);
-    $(".logged-in-ctx").css("left", pos.left);
+    $(".logged-in-ctx").css("top", pos.top + 25);
+    $(".logged-in-ctx").css("left", pos.left - 110);
 }
 
 function toggleNavOpts() {
